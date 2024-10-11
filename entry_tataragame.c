0129502264054b2e962aedf438d1b09047056132
Tesemir <madi41674@gmail.com>
2024-10-11 13:52:26 +0500

    Camera, sprite system and entity structure

--- entry_tataragame.c
+++ entry_tataragame.c
@@ -1,14 +1,56 @@
+bool almost_equals(float a, float b, float epsilon) {
+	return fabs(a - b) <= epsilon;
+}
+
+bool animate_f32_to_target(float* value, float target, float delta_t, float rate) {
+	*value += (target - *value) * (1.0 - pow(2.0f, -rate * delta_t));
+	if (almost_equals(*value, target, 0.001f))
+	{
+		*value = target;
+		return true;
+	}
+	return false;
+}
+
+void animate_v2_to_target(Vector2* value, Vector2 target, float delta_t, float rate) {
+	animate_f32_to_target(&(value->x), target.x, delta_t, rate);
+	animate_f32_to_target(&(value->y), target.y, delta_t, rate);
+}
+
+typedef struct Sprite {
+	Gfx_Image* image;
+	Vector2 size;
+} Sprite;
+typedef enum Sprite_ID {
+	SPRITE_nil,
+	SPRITE_player,
+	SPRITE_rock0,
+	SPRITE_tree0,
+	SPRITE_tree1,
+	SPRITE_MAX,
+} Sprite_ID;
+Sprite* sprites[SPRITE_MAX];
+Sprite* get_sprite(Sprite_ID id) {
+	if (id >= 0 && id < SPRITE_MAX) {
+		return sprites[id];
+	}
+	return sprites[0];
+}
+
 typedef enum EntityArchetype {
-	nil = 0,
-	rock = 1,
-	tree = 2,
-	player = 3,
+	arch_nil = 0,
+	arch_rock = 1,
+	arch_tree = 2,
+	arch_player = 3,
 } EntityArchetype;
 
 typedef struct Entity {
 	bool is_valid;
 	EntityArchetype arch;
 	Vector2 pos;
+
+	bool render_sprite;
+	Sprite_ID sprite_id;
 } Entity;
 
 #define MAX_ENTITY_COUNT 1024
@@ -29,6 +71,7 @@ Entity* entity_create() {
 		}
 	}
 	assert(entity_found, "No!");
+	entity_found->is_valid = true;
 	return entity_found;
 }
 
@@ -36,6 +79,19 @@ void entity_destroy(Entity* entity) {
 	memset(entity, 0, sizeof(Entity));
 }
 
+void setup_rock(Entity *en) {
+	en->arch = arch_rock;
+	en->sprite_id = SPRITE_rock0;
+}
+void setup_tree(Entity *en) {
+	en->arch = arch_tree;
+	en->sprite_id = SPRITE_tree0;
+	// en->sprite_id = SPRITE_tree1;
+}
+void setup_player(Entity *en) {
+	en->arch = arch_player;
+	en->sprite_id = SPRITE_player;
+}
 
 int entry(int argc, char **argv) {
 	
@@ -46,34 +102,68 @@ int entry(int argc, char **argv) {
 
 	world = alloc(get_heap_allocator(), sizeof(World));
 
-	Gfx_Image* player = load_image_from_disk(STR("Assets/player.png"), get_heap_allocator());
-	assert(player, "fuckie wucky shpagi");
-	Gfx_Image* tree0 = load_image_from_disk(STR("Assets/tree0.png"), get_heap_allocator());
-	assert(tree0, "fuckie wucky shpagi");
-	Gfx_Image* tree1 = load_image_from_disk(STR("Assets/tree1.png"), get_heap_allocator());
-	assert(tree1 , "fuckie wucky shpagi");
-	Gfx_Image* rock0 = load_image_from_disk(STR("Assets/rock0.png"), get_heap_allocator());
-	assert(rock0, "fuckie wucky shpagi");
+	sprites[SPRITE_player] = &(Sprite) { .image = load_image_from_disk(STR("Assets/player.png"), get_heap_allocator()), .size = v2(6.0, 8.0)};
+	sprites[SPRITE_rock0] = &(Sprite) { .image = load_image_from_disk(STR("Assets/rock0.png"), get_heap_allocator()), .size = v2(8.0, 4.0)};
+	sprites[SPRITE_tree0] = &(Sprite) { .image = load_image_from_disk(STR("Assets/tree0.png"), get_heap_allocator()), .size = v2(11.0, 19.0)};
 
 	Entity* player_en = entity_create();
-	Entity* rock_en = entity_create();
+	setup_player(player_en);
 	
+	for (int i = 0; i < 30; i++) {
+		Entity* en = entity_create();
+		setup_rock(en);
+		en->pos = v2(get_random_float32_in_range(-200, 200), get_random_float32_in_range(-200, 200));
+	}
+
+	for (int i = 0; i < 30; i++) {
+		Entity* en = entity_create();
+		setup_tree(en);
+		en->pos = v2(get_random_float32_in_range(-200, 200), get_random_float32_in_range(-200, 200));
+	}
+
 	float64 seconds_counter = 0.0;
 	s32 frame_count = 0;
 	float64 last_time = os_get_elapsed_seconds();
 
+	float zoom = 5.3;
+	Vector2 camera_pos = v2(0, 0);
+
 	while (!window.should_close) {
 		reset_temporary_storage();
-
-		draw_frame.projection = m4_make_orthographic_projection(window.width * -0.5, window.width * 0.5, window.height * -0.5, window.height * 0.5, -1, 10);
-
-		float zoom = 5.3;
-		draw_frame.camera_xform = m4_make_scale(v3(1.0/zoom, 1.0/zoom, 1.0));
-
 		float64 now = os_get_elapsed_seconds();
 		float64 delta_t = now - last_time;
 		last_time = now;
+		os_update();
+		
+		draw_frame.projection = m4_make_orthographic_projection(window.width * -0.5, window.width * 0.5, window.height * -0.5, window.height * 0.5, -1, 10);
 
+		// Camera
+		{
+			Vector2 target_pos = player_en->pos;
+			animate_v2_to_target(&camera_pos, target_pos, delta_t, 15.0f);
+
+			draw_frame.camera_xform = m4_scalar(1.0);
+			draw_frame.camera_xform = m4_mul(draw_frame.camera_xform, m4_make_translation(v3(camera_pos.x, camera_pos.y, 0)));
+			draw_frame.camera_xform = m4_mul(draw_frame.camera_xform, m4_make_scale(v3(1.0/zoom, 1.0/zoom, 1.0)));
+		} 
+
+		for (int i = 0; i < MAX_ENTITY_COUNT; i++) {
+			Entity* en = &world->entities[i];
+			if (en->is_valid) {
+				switch (en->arch) {
+					default: 
+					{
+						Sprite* sprite = get_sprite(en->sprite_id);
+
+						Matrix4 xform = m4_scalar(1.0);
+						xform         = m4_translate(xform, v3(en->pos.x, en->pos.y, 0));
+						xform         = m4_translate(xform, v3(sprite->size.x * -0.5, sprite->size.y * -0.5, 0));
+						draw_image_xform(sprite->image, xform, sprite->size, COLOR_WHITE);
+						break;
+					}
+				}
+			}
+		}
 
 		if (is_key_just_pressed(KEY_ESCAPE)) {
 			window.should_close = true;
@@ -94,24 +184,8 @@ int entry(int argc, char **argv) {
 		}
 		input_axis = v2_normalize(input_axis);
 
-		player_en->pos = v2_add(player_en->pos, v2_mulf(input_axis, 50.0 * delta_t));
+		player_en->pos = v2_add(player_en->pos, v2_mulf(input_axis, 70.0 * delta_t));
 
-		{
-			Vector2 size = v2(6.0, 8.0);
-			Matrix4 xform = m4_scalar(1.0);
-			xform         = m4_translate(xform, v3(player_en->pos.x, player_en->pos.y, 0));
-			xform         = m4_translate(xform, v3(size.x * -1, 0, 0));
-			draw_image_xform(player, xform, size, COLOR_WHITE);
-		}
-
-		{
-			Vector2 size = v2(11.0, 19.0);
-			Matrix4 xform = m4_scalar(1.0);
-			xform         = m4_translate(xform, v3(size.x, 0.0, 0));
-			draw_image_xform(tree0, xform, size, COLOR_WHITE);
-		}
-
-		os_update(); 
 		gfx_update();
 		seconds_counter += delta_t;
 		frame_count += 1;
